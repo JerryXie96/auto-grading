47582d322d68adecce98682c8bae01cab0e7d3f4
Jerry Xie <hongcheng.xie.hk@gmail.com>
2023-12-02 07:46:45 +0800

    development on Dec-1

--- auto-grading.py
+++ auto-grading.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from typing import List
+from typing import Dict
 
 import argparse
 import sys
@@ -11,6 +11,30 @@ import yaml
 def err_exit(message: str) -> None:
     print(message, file=sys.stderr)
     exit(1)
+    
+def read_csv(config: Dict, used_field: str) -> pd.DataFrame:
+    try:
+        ret = pd.read_csv(config[used_field]['file'], delimiter=config[used_field]['delimiter'])
+    except:
+        err_exit('Cannot load files about ' + used_field)
+    return ret
+
+def student_ans_preprocessing(config: Dict) -> Dict:
+    students_ans = {}
+    students_ans_csv = read_csv(config, 'std_answers')
+    
+    for idx, row in students_ans_csv.iterrows():
+        student_id_keys = []
+        answers_id_keys = []
+        for key in row.keys():
+            if key.startswith(config['std_answers']['answer_prefix']):
+                answers_id_keys.append(key.split('.')[1])
+            elif key.startswith(config['std_answers']['id_prefix']):
+                student_id_keys.append(key.split('.')[1])
+        student_id_keys.sort()
+        answers_id_keys.sort()
+    
+    return students_ans
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Auto-grading tool with FormScanner')
@@ -23,10 +47,5 @@ if __name__ == '__main__':
     except:
         err_exit('Cannot load the config file.')
 
-    try:
-        students_ans = pd.read_csv(config['std_answers']['file'], delimiter=config['std_answers']['delimiter'])
-    except:
-        err_exit('Cannot load student answers.')
-    
-    
+    student_ans = student_ans_preprocessing(config)
     
